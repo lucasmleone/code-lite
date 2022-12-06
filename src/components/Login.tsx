fe2a060aac1aa3dd36f2798e1eb128affadbfd3e
Lucas Leone <lucasmati9@gmail.com>
2022-12-06 02:10:44 +0100

    funcional and design done

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,53 +1,72 @@
 import { FC, useState } from 'react';
 import { toast } from 'react-toastify';
-
 import useAPI from '../hooks/useAPI';
+
 
 const Login: FC = () => {
   const { getUser } = useAPI();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+async function onLoggin(e:React.FormEvent<HTMLFormElement>) {
+   e.preventDefault();
+   if(!email || !password){
+    toast('Please complete all fields', { type: 'error' })
+  }else{
+    const user=  await getUser(email,password)
+    toast(`Login successful ${user.email}`, { type: 'success' })
+  }
+    
+
+}
+
   return (
     <>
-      <div className="bg-gray-900 px-20 text-white py-10">
-        <h1 className="text-lg font-semibold">Sign in</h1>
-      </div>
-      <div className="px-20 text-white py-10 text-base max-w-2xl">
-        To the VINCI code test. Please have a look at the README.md.
-        <br />
-        <br />
-        Good  luck!
-        <br />
-        <i>- VINCI</i>
-        <form
-          className="flex flex-col mt-12"
-          onSubmit={async (e) => {
-            e.preventDefault();
+      <div className="grid  grid-cols-1 mobile:grid-cols-2 mx-auto min-h-screen">
 
-            toast('I am incomplete 😢', { type: 'error' });
-          }}
-        >
-          <input
-            type="email"
-            className="mt-2 text-black"
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-            value={email}
-            placeholder="Email"
-          />
-          <input
-            type="password"
-            className="mt-2 text-black"
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            value={password}
-            placeholder="Password"
-          />
-          <input className="mt-2 hover:bg-white/20 cursor-pointer" type="submit" value="Sign in" />
-        </form>
+        <div className="min-m-screen grid place-content-center">
+          <div className='flex justify-center'>
+            <p className='text-xl font-semibold'>VINCI</p>
+            <span className='self-start text-sm text-vinci-main'>BETA</span>
+          </div>
+          <form
+            className='flex flex-col'
+            onSubmit={onLoggin}
+          >
+            <input
+              type="email"
+              className="p-2 w-96 mt-[46px] h-[54px] text-black text-lg border-vinci-gray border-2 rounded-vc"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              value={email}
+              placeholder="Email"
+            />
+            <input
+              type="password"
+              className="p-2 w-96 h-[54px] mt-4 text-black text-lg border-vinci-gray border-2 rounded-vc"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              value={password}
+              placeholder="Password"
+            />
+
+            <div className='flex justify-between mt-4 '>
+              <label className='text-lg'> <input className='text-lg' type='checkbox' name='rememberMe' id='rememberMe' /> Remember me</label>
+              <a href='#' className='text-lg self-end' >Forgot Password?</a>
+            </div>
+
+            <input className="text-white mt-6 bg-vinci-main cursor-pointer rounded-vc text-base font-semibold h-[54px] hover:text-vinci-main hover:bg-vinci-gray" type="submit" value="Continue" />
+          </form>
+          <hr className="h-px bg-vinci-gray mt-[30px] border-0 "/>
+
+          <div className=' mt-4 flex justify-center'>
+          <p className='text-vinci-gray-dark'>Don’t have an account?</p>
+          <a className='text-vinci-main cursos-pointer underline' href='#'>Sign up</a>
+          </div>
+        </div>
+        <div className='bg-pyramid bg-cover'></div>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'pyramid':"url('../public/background.png')"
+      },
+      fontFamily:{
+        'oakesGrotesk':['OakesGrotesk','serif']
+      },
+      colors:{
+        'vinci-main': '#6D3AF9',
+        'vinci-light':'#BBA3F5',
+        'vinci-gray':'#DADADA',
+        'vinci-gray-dark':'#9A9696'
+      },
+      borderRadius:{
+        'vc':'10px'
+      }     
+    },
+    fontSize: {
+      sm: ['10px', '10px'],
+      base: ['14px', '14px'],
+      lg: ['18px', '18px'],
+      xl: ['34px', '34px'],
+    },
+    screens:{
+      'mobile':'768px'
+    }
   },
   plugins: [],
 };
